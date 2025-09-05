9b87ab8fa8d501b15db0f1a9e4736c18367f5596
KK <kashiwagik@adm.ncn.ac.jp>
2025-09-05 23:10:18 +0900

    fix

--- docs/script.js
+++ docs/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
             // 定数
             this.weekdays = ['日', '月', '火', '水', '木', '金', '土'];
             this.undergradGrades = ["1年生", "2年生", "3年生", "4年生", "4年助産"];
-            this.graduateGrades = ["M1", "M2", "D1", "D2/3"];
+            this.graduateGrades = ["M1", "M2", "D1", "D2/D3"];
             this.allGrades = this.undergradGrades.concat(this.graduateGrades);
 
             // 更新フラグ（無限ループ防止）
